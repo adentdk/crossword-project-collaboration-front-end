5bbd59e9817226b3e694d6392daa898e03544b11
= <=>
2019-07-10 21:38:18 +0700

    .

---
+++ assets/dummyAnswer.js
@@ -0,0 +1,29 @@
+[
+    {
+        "id": 1,
+        "crossword_id": 1,
+        "number": 1,
+        "question": "hewan berkaki empat",
+        "answer": "jaran",
+        "is_clue": true,
+        "indexes": '0,1,2,3,4'
+    },
+    {
+        "id": 2,
+        "crossword_id": 1,
+        "number": 2,
+        "question": "hewan berkaki dua",
+        "answer": "ayam",
+        "is_clue": false,
+        "indexes": '1,6,11,16'
+    },
+    {
+        "id": 3,
+        "crossword_id": 1,
+        "number": 3,
+        "question": "hewan yang seperti chandra",
+        "answer": "ampas",
+        "is_clue": false,
+        "indexes": '15,16,17,18,19'
+    },
+]
